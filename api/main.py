9ca72982b50163ce7b4f48cbf093ad92dc033fe1
Ashad Qureshi <ashad001sp@gmail.com>
2024-06-08 11:20:55 +0500

    refactored

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 """
 https://python.langchain.com/v0.2/docs/integrations/chat/groq/
 """
-from CREDS import GROQ_API_KEY
+from api.CREDS import GROQ_API_KEY
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
@@ -9,7 +9,7 @@ from langchain_groq import ChatGroq
 
 llm = ChatGroq(api_key=GROQ_API_KEY, model="llama3-8b-8192")
 
-with open("./prompts/system_prompt.txt", "r") as f:
+with open("./api/prompts/system_prompt.txt", "r") as f:
     system = f.read()
     
 human = "{text}"
@@ -19,10 +19,10 @@ prompt = ChatPromptTemplate.from_messages([
 ])
 chain = prompt | llm
 
-with open("./chats/good_chat.txt", "r") as f:
+with open("./api/chats/good_chat.txt", "r") as f:
     good_chat = f.read()
 
-with open("./chats/avg_chat.txt", "r") as f:
+with open("./api/chats/avg_chat.txt", "r") as f:
     bad_chat = f.read()
 
 res = chain.invoke(good_chat)
