abc456c718aeb35d166a995e40263ec43aadaa2c
Ashad Qureshi <ashad001sp@gmail.com>
2024-07-20 16:26:10 +0500

    api v3.0

--- hf-api/api/api.py
+++ hf-api/api/api.py
@@ -3,6 +3,7 @@ import re
 import json
 from typing import List, Dict
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import PromptTemplate
@@ -12,15 +13,17 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_groq import ChatGroq
 # from api.CREDS import GROQ_API_KEY
 
+load_dotenv()
+
 class ChatOutput(BaseModel):
     score: int = Field(description="score of chats out of 100")
     description: str = Field(description="Short description on why the score was given, and also suggest tips for user on how could he/she make it interactive and better.")
-    messages: List[Dict[str, str]] = Field(description="Create a list of 3 potential messages to make the chat better and more interesting. Feel free to suggest jokes or share some fun facts to make the conversation more interactive and healthy. Each message should include a username and message key value.")
-    
+    messages: List[str] = Field(description="Create a list of 5 potential messages for the person (self) to make the chat better and more interesting. Feel free to suggest jokes or share some fun facts to make the conversation more interactive and healthy. (just strings in a list and NOTHING else)")
 
 app = FastAPI()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+print("--> ", GROQ_API_KEY)
 
 llm = ChatGroq(api_key=GROQ_API_KEY, model="llama3-8b-8192")
 with open("./api/prompts/system_prompt.txt", "r") as f:
@@ -37,20 +40,23 @@ prompt = PromptTemplate(
 )
 chain = prompt | llm | parser
 
-print(parser.get_format_instructions())
 @app.post("/chat/")
 async def chat(input_data: ChatOutput):
     res = chain.invoke({"user_query": input_data['text']})
     if res is None:
         return {"score": 0, "description": "No response found."}
 
-    if isinstance(res, dict):
-        return res
-
     if isinstance(res, str):
-        # get dict object between ``` and ``` in the string
         res = re.search(r'```(.*?)```', res, re.DOTALL)
-
-    return res
+        if not res:
+            res = re.search(r'---(.*?)---', res, re.DOTALL)        
+            
+        if res:
+            res_str = res.group(1).replace("'", '"')  
+        try:
+            res = json.loads(res_str)  
+        except json.JSONDecodeError:
+            return {"score": 0, "description": "Invalid JSON format."}
     
-    # return {"response": res}
+    print(f"Response type: {type(res)} --> {res}")
+    return res
